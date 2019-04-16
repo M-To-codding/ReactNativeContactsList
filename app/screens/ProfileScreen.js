f8d9e6a81d5ca79027a0fceeb1ae513f97c7d3a9
Marina <marina.todorishina@gmail.com>
2019-04-16 19:33:34 +0300

    change layout structure, store contact in async storage, add buttons to profile screen

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -13,13 +13,18 @@ import {
   StatusBar,
   RefreshControl,
   TextInput,
-  Picker
+  Picker,
+  TouchableHighlight,
+  Linking
 } from 'react-native';
 import {Avatar, Header, Icon, List} from 'react-native-elements';
 import {NavigationActions, HeaderBackButton} from 'react-navigation';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import HeaderImageScrollView, {TriggeringView} from 'react-native-image-header-scroll-view';
 import DatePicker from 'react-native-datepicker';
+
+import profileStyles from '../assets/styles/Profile';
+import storeData from '../actions/storeDataInAsyncStorage';
 
 
 export default class ProfileScreen extends React.Component {
@@ -30,7 +35,7 @@ export default class ProfileScreen extends React.Component {
     console.log('Hello world!');
     console.log(props);
 
-    let user = props.navigation.state.params.user
+    let user = props.navigation.state.params.user;
 
     this.state = {
       user: user,
@@ -38,7 +43,9 @@ export default class ProfileScreen extends React.Component {
       name: user.name.first,
       lastName: user.name.last,
       gender: user.gender,
-      date: user.dob.date
+      date: user.dob.date,
+      phone: user.phone,
+      email: user.email
     }
   }
 
@@ -82,10 +89,51 @@ export default class ProfileScreen extends React.Component {
     return true;
   }
 
+  saveContact(contact){
+    storeData(contact);
+  }
+
+   handleProfileButtons() {
+    const buttons =
+        <View style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: wp('100%'),
+          backgroundColor: '#000',
+          height: 100,
+          marginLeft: -10,
+          marginRight: -10,
+          marginBottom: 25
+        }}>
+          <TouchableOpacity style={profileStyles.button} onPress={()=>Linking.openURL(`tel:${this.state.phone}`)}>
+            <Image
+              source={{uri: 'https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/phone-icon-18-256.png'}}
+              style={{width: 30, height: 30}}/>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={profileStyles.button} onPress={()=>Linking.openURL('mailto:support@example.com')}>
+            <Image source={{uri: 'https://cdn.pixabay.com/photo/2013/04/01/21/31/mail-99218_640.png'}}
+                   style={{width: 30, height: 30}}/>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={profileStyles.button} onPress={()=>this.saveContact(this.state.user)}>
+            <Image
+              source={{uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbUYw0HQJzifmBIw49AqI5y82JUnP8EZKt2Pw0eE_at-vRJ4YRDg'}}
+              style={{width: 30, height: 30}}/>
+          </TouchableOpacity>
+        </View>;
+
+    return buttons;
+  }
+
   render() {
+
+    const profileButtons = this.handleProfileButtons();
+
     return (
 
-      <View style={style.container}>
+      <View style={profileStyles.container}>
         <StatusBar
           hidden={false}
           animated={true}
@@ -97,11 +145,13 @@ export default class ProfileScreen extends React.Component {
           maxHeight={250}
           minHeight={75}
           disableHeaderGrow={false}
+          bounces={true}
           overlayColor="#2b2b2b"
           style={{width: wp("100%")}}
           maxOverlayOpacity={0.95}
           minOverlayOpacity={0.3}
-          fadeOutForeground
+          fadeOutForeground={true}
+          foregroundParallaxRatio={2}
           headerImage={{uri: this.state.user.picture.large}}
           refreshControl={
             <RefreshControl
@@ -112,72 +162,124 @@ export default class ProfileScreen extends React.Component {
           }
           scrollViewBackgroundColor="#fff">
 
-          <View style={[style.detailsContainer, {minHeight: hp('83%')}]}>
+          <View style={[profileStyles.detailsContainer, {minHeight: hp('83%')}]}>
             <TriggeringView onHide={() => console.log("text hidden")}>
-              <Text>Name: </Text>
-              <TextInput
-                style={{height: 40, paddingHorizontal: 10}}
-                onChangeText={(name) => this.setState({name})}
-                underlineColorAndroid={'#c1c1c1'}
-                value={this.state.name}
-                shake={true}
-              />
-              <Text>Surname: </Text>
-              <TextInput
-                style={{height: 40, paddingHorizontal: 10}}
-                onChangeText={(lastName) => this.setState({lastName})}
-                underlineColorAndroid={'#c1c1c1'}
-                value={this.state.lastName}
-                shake={true}
-              />
 
-              <Picker
-                selectedValue={this.state.gender}
-                style={{height: 50, width: 100, borderBottomColor: '#c1c1c1', borderBottomWidth: 1}}
-                underlineColorAndroid={'#c1c1c1'}
-                onValueChange={(itemValue, itemIndex) =>
-                  this.setState({gender: itemValue})
-                }>
-                <Picker.Item label={this.state.gender} value={this.state.gender}/>
-                <Picker.Item label={this.state.gender === 'female' ? 'male' : 'female'}
-                             value={this.state.gender === 'female' ? 'male' : 'female'}/>
-              </Picker>
+              {profileButtons}
 
-              <Text>Birthday: </Text>
-              <DatePicker
-                style={{width: 200}}
-                date={this.state.date}
-                mode="date"
-                placeholder="select date"
-                format="YYYY-MM-DD"
-                minDate="2016-05-01"
-                maxDate="2016-06-01"
-                confirmBtnText="Confirm"
-                cancelBtnText="Cancel"
-                customStyles={{
-                  dateIcon: {
-                    position: 'absolute',
-                    display:'none',
-                    left: 0,
-                    top: 4,
-                    marginLeft: 0
-                  },
-                  dateInput: {
-                    border: 0,
-                    borderWidth: 0,
+              <View>
+                <Text style={profileStyles.labelStyle}>Name</Text>
+                <TextInput
+                  label="Name"
+                  style={profileStyles.textInput}
+                  onChangeText={(name) => this.setState({name})}
+                  underlineColorAndroid={"#c1c1c1"}
+                  value={this.state.name}
+                  shake={true}
+                />
+              </View>
+
+              <View>
+                <Text style={profileStyles.labelStyle}>Surname</Text>
+                <TextInput
+                  style={profileStyles.textInput}
+                  label="Surname"
+                  onChangeText={(lastName) => this.setState({lastName})}
+                  underlineColorAndroid={"#c1c1c1"}
+                  value={this.state.lastName}
+                  shake={true}
+                />
+              </View>
+
+              <View style={{flexDirection: "row"}}>
+                <View>
+                  <Picker
+                    selectedValue={this.state.gender}
+                    style={[
+                      profileStyles.textInput,
+                      {
+                        height: 50,
+                        width: wp('48%'),
+                        borderBottomWidth: 1,
+                        borderColor: '#c1c1c1',
+                      }]}
+                    itemStyle={{
+                      borderTopWidth: 1,
+                      borderTopColor: '#000',
+                      borderBottomWidth: 1,
+                      borderBottomColor: '#000'
+                    }}
+                    underlineColorAndroid={'#c1c1c1'}
+                    onValueChange={(itemValue, itemIndex) =>
+                      this.setState({gender: itemValue})
+                    }>
+                    <Picker.Item label={this.state.gender} value={this.state.gender}/>
+                    <Picker.Item label={this.state.gender === 'female' ? 'male' : 'female'}
+                                 value={this.state.gender === 'female' ? 'male' : 'female'}/>
+                  </Picker>
+
+                  {/*Some solution for android picker border style*/}
+                  <View style={{
+                    height: 1,
+                    width: wp('48%'),
                     borderBottomWidth: 1,
                     borderColor: '#c1c1c1',
-                    marginLeft: 36
-                  }
-                }}
-                onDateChange={(date) => {this.setState({date: date})}}
-              />
+                    marginTop: -21,
+                    marginBottom: 21
+                  }}/>
+                  {/**/}
 
-              <Text>{this.state.user.dob.date}</Text>
-              <Text>Phone: </Text>
-              <Text>{this.state.user.phone}</Text>
-              <Text>E-mail: </Text>
-              <Text>{this.state.user.email}</Text>
+                </View>
+
+                <View>
+                  <Text style={profileStyles.labelStyle}>Birthday: </Text>
+                  <DatePicker
+                    date={this.state.date}
+                    mode="date"
+                    placeholder="select date"
+                    format="YYYY-MM-DD"
+                    confirmBtnText="Confirm"
+                    cancelBtnText="Cancel"
+                    showIcon={false}
+                    customStyles={{
+                      dateInput: [profileStyles.textInput, {
+                        border: 0,
+                        borderWidth: 0,
+                        borderBottomWidth: 1,
+                        borderColor: '#c1c1c1',
+                        alignItems: 'stretch',
+                        justifyContent: 'flex-end',
+                        width: wp('48%'),
+                      }],
+                    }}
+                    onDateChange={(date) => {
+                      this.setState({date: date})
+                    }}
+                  />
+                </View>
+              </View>
+
+              <View>
+                <Text style={profileStyles.labelStyle}>Phone: </Text>
+                <TextInput
+                  style={profileStyles.textInput}
+                  onChangeText={(phone) => this.setState({phone})}
+                  underlineColorAndroid={'#c1c1c1'}
+                  value={this.state.phone}
+                  shake={true}
+                />
+              </View>
+
+              <View>
+                <Text style={profileStyles.labelStyle}>E-mail: </Text>
+                <TextInput
+                  style={profileStyles.textInput}
+                  onChangeText={(email) => this.setState({email})}
+                  underlineColorAndroid={'#c1c1c1'}
+                  value={this.state.email}
+                  shake={true}
+                />
+              </View>
             </TriggeringView>
           </View>
 
@@ -187,31 +289,3 @@ export default class ProfileScreen extends React.Component {
     );
   }
 }
-
-const style = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ecf0f1',
-    alignItems: 'center',
-  },
-  scrollContainer: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#000000',
-    marginTop: 300
-  },
-  buttonsPanel: {
-    backgroundColor: '#b6d4ff'
-  },
-  bgImage: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-    resizeMode: 'cover',
-  },
-  detailsContainer: {
-    paddingHorizontal: 10
-  }
-});
