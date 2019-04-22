45bd46af229bb411fb79e57019cfa2406c9af395
Marina <marina.todorishina@gmail.com>
2019-04-22 18:58:27 +0300

    separate buttons styles, add new parameter to buttons data arrey

---
+++ app/assets/styles/ProfileButtons.js
@@ -0,0 +1,32 @@
+import {Platform, StyleSheet} from "react-native";
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+
+let buttonsStyles;
+
+export default buttonsStyles = StyleSheet.create({
+  button: {
+    width: wp('33%'),
+    height: 70,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 20,
+    borderRadius: 30,
+    margin: 10,
+    elevation: 4,
+  },
+  disabledBtn: {
+    opacity: 0.2
+  },
+  btnsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: wp('100%'),
+    backgroundColor: '#000',
+    height: 70,
+    marginLeft: -10,
+    marginRight: -10,
+    marginBottom: 15
+  }
+});
--- app/data/buttons.js
+++ app/data/buttons.js
@@ -1,24 +1,24 @@
-import profileStyles from "../assets/styles/Profile";
+import buttonsStyles from "../assets/styles/ProfileButtons";
 
 
 let btnsData = {};
 
-export function setData(props, saveContact) {
+export function setData(props, saveContact, removeContact) {
   return [
     {
-      btnStyle: profileStyles.button,
+      btnStyle: buttonsStyles.button,
       linkingUrl: `tel:${props.phone}`,
       img: 'https://iconsplace.com/wp-content/uploads/_icons/ffffff/256/png/phone-icon-18-256.png',
       imgStyle: {width: 20, height: 20}
     },
     {
-      btnStyle: profileStyles.button,
+      btnStyle: buttonsStyles.button,
       linkingUrl: `mailto:${props.email}`,
       img: 'https://cdn.pixabay.com/photo/2013/04/01/21/31/mail-99218_640.png',
       imgStyle: {width: 20, height: 20}
     },
     {
-      btnStyle: [profileStyles.button],
+      btnStyle: [buttonsStyles.button],
       onPressBtn: saveContact,
       img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbUYw0HQJzifmBIw49AqI5y82JUnP8EZKt2Pw0eE_at-vRJ4YRDg',
       imgStyle: {width: 20, height: 20}
