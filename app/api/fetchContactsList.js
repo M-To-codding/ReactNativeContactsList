b9572d3a015ebd8829d9cf291008c4cd737b75e3
Marina <marina.todorishina@gmail.com>
2019-05-02 17:51:41 +0300

    update settings data in fetchContactList

--- app/api/fetchContactsList.js
+++ app/api/fetchContactsList.js
@@ -1,5 +1,35 @@
+import {AsyncStorage} from 'react-native';
+import settings from './../data/settings';
+import getAppSettings from './../actions/getAppSettings';
+
+
+let contactsCount = 10;
+let url = `https://randomuser.me/api/?results=${contactsCount}&noinfo`;
+
+function getContactsSettings() {
+
+  getAppSettings().then((response) => {
+
+    if (response) {
+      let data = JSON.parse(response)
+
+      return contactsCount = parseInt(data.recommended.selectedCount);
+    }
+  })
+
+}
+getContactsSettings();
+
+
 export default function () {
-  return fetch('https://randomuser.me/api/?results=10&noinfo')
+  getContactsSettings();
+
+  url = `https://randomuser.me/api/?results=${contactsCount}&noinfo`;
+
+  console.log('fetchresponse')
+  console.log(url)
+
+  return fetch(url)
     .then(response => response.json())
     .then((data) => {
       // handledData = JSON.stringify(data);
