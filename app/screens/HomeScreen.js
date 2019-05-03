227dc570b5fab3f96a7bc5a5a717b8c89ea50b33
Marina <marina.todorishina@gmail.com>
2019-05-03 19:55:55 +0300

    change template parts in home screen

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -11,12 +11,12 @@ import {
 } from 'react-native';
 import {WebBrowser} from 'expo';
 import deepDiffer from 'react-native/lib/deepDiffer';
+import {NavigationActions} from "react-navigation";
 
 import {MonoText} from '../components/StyledText';
 import homeStyles from '../assets/styles/App';
 import getData from '../actions/getDataFromAsyncStorage';
 import {ContactsList} from "../components/ContactsList/ContactsList";
-import {NavigationActions} from "react-navigation";
 
 
 export default class HomeScreen extends React.Component {
@@ -82,7 +82,6 @@ export default class HomeScreen extends React.Component {
 
     var that = this;
     getData().then((response) => {
-      console.log('checkUpdates');
 
       console.log(JSON.parse(response))
       if (this._isMounted && response) {
@@ -114,26 +113,19 @@ export default class HomeScreen extends React.Component {
   render() {
     const {contacts, loading, error} = this.state;
 
-    if (loading) return (<View style={styles.welcomeContainer}><Text>Home</Text><Text> Loading...</Text></View>);
+    if (loading) return (<View style={styles.welcomeContainer}>
+      <Image source={
+        __DEV__
+          ? require('../assets/images/robot-dev.png')
+          : require('../assets/images/robot-prod.png')
+      }
+      style={styles.welcomeImage}
+    />
+      <Text> Loading...</Text></View>);
 
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-
-          <View style={styles.welcomeContainer}>
-            <Image
-              source={
-                __DEV__
-                  ? require('../assets/images/robot-dev.png')
-                  : require('../assets/images/robot-prod.png')
-              }
-              style={styles.welcomeImage}
-            />
-
-            <Text>
-              Home
-            </Text>
-          </View>
 
           <View>
             <ContactsList navigation={this.props.navigation} savedContacts={contacts} isSavedContacts={true}
