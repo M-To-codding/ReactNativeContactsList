1260532f733f05f89f959bc9c538591b0a7e25cc
Marina <marina.todorishina@gmail.com>
2019-05-03 19:54:15 +0300

    fix settings model, add field language

--- app/data/settings.js
+++ app/data/settings.js
@@ -1,7 +1,7 @@
 const settings = {
   recommended: {
     count: ['5', '10', '15', '20'],
-    selectedCount: '10 users'
+    selectedCount: '10'
   },
   notifications: {
     isShown: false,
@@ -11,10 +11,7 @@ const settings = {
       selectedCount: '15'
     },
   },
-  languge: {
-    count: ['15', '30', '45', '60'],
-    selectedCount: '15'
-  },
+  language: 'en'
 }
 
 export default settings;
