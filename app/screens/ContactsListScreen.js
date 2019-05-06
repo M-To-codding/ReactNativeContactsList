d3a08c2d162d6362f693a7e3d6dd7133a70fca4b
Marina <marina.todorishina@gmail.com>
2019-05-06 18:16:21 +0300

    add floating button to contacts list screen, clean the code, change indents

--- app/screens/ContactsListScreen.js
+++ app/screens/ContactsListScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {FlatList, RefreshControl, ScrollView, StyleSheet, View} from 'react-native';
-import { ExpoLinksView } from '@expo/samples';
+import {ExpoLinksView} from '@expo/samples';
 import {ContactsList} from "../components/ContactsList/ContactsList";
+import FloatingAddContactButton from "../components/FloatingAddContactButton";
+import AppNavigator from "../navigation/AppNavigator";
 
 export default class ContactsListScreen extends React.Component {
   constructor(props) {
@@ -23,7 +25,7 @@ export default class ContactsListScreen extends React.Component {
       refreshList: true
     })
 
-    setTimeout(()=>{
+    setTimeout(() => {
       this.setState({
         refreshing: false,
         refreshList: false
@@ -32,28 +34,29 @@ export default class ContactsListScreen extends React.Component {
   }
 
   checkUpdates(updated) {
-
-    this.forceUpdate()
-    if(updated) {
-    }
+    this.forceUpdate();
   }
 
   render() {
     return (
-      <ScrollView style={styles.container}
-                  refreshControl={
-                    <RefreshControl
-                      onRefresh={()=>this._onRefresh()}
-                      refreshing={this.state.refreshing}
-                      tintColor="white"
-                    />
-                  }>
 
-        <View>
-          <ContactsList navigation={this.props.navigation} refreshTheList={this.state.refreshList} checkUpdates={this.checkUpdates.bind(this)}> </ContactsList>
-        </View>
+      <View style={styles.container}>
+        <ScrollView style={styles.container}
+                    refreshControl={
+                      <RefreshControl
+                        onRefresh={() => this._onRefresh()}
+                        refreshing={this.state.refreshing}
+                        tintColor="white"
+                      />
+                    }>
+          <ContactsList navigation={this.props.navigation} refreshTheList={this.state.refreshList}
+                        checkUpdates={this.checkUpdates.bind(this)}> </ContactsList>
 
-      </ScrollView>
+        </ScrollView>
+
+        <FloatingAddContactButton navigation={this.props.navigation} isNewContact={true} checkUpdates={this.checkUpdates.bind(this)}/>
+
+      </View>
     );
   }
 }
