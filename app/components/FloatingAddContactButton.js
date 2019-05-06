1df13d281c23f782a1f30dd4974623bf09f38f6b
Marina <marina.todorishina@gmail.com>
2019-05-06 18:02:37 +0300

    fix floating button, add handler for navigating to empty profile

--- app/components/FloatingAddContactButton.js
+++ app/components/FloatingAddContactButton.js
@@ -1,41 +1,73 @@
 import React from 'react';
-import {Image, Linking, TouchableOpacity, View} from "react-native";
+import {Image, Linking, TouchableOpacity, View, StyleSheet} from "react-native";
 import {Icon} from "react-native-elements";
 import deepDiffer from 'react-native/lib/deepDiffer'
 
 import buttonsStyles from "../assets/styles/ProfileButtons";
+import profileStyles from "../assets/styles/Profile";
 
 
 export default class FloatingAddContactButton extends React.Component {
+  constructor(props) {
+    super(props);
 
-  render() {
-
-    return (
-      <View style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#000',
-      }}>
-        <TouchableOpacity style={{
-          position: 'absolute',
-          width: 150,
-          height: 40,
-          borderRadius: 20,
-          flex: 1,
-          alignItems: 'center',
-          justifyContent: 'center',
-          right: 30,
-          bottom: 30,
-        }}>
-          <Icon
-            name='add'
-            color='#fff'
-            style={{resizeMode: 'contain',}}
-          />
-        </TouchableOpacity>
-      </View>
-    );
+    this.state = {
+      shouldUpdate: false
+    }
   }
 
+  clickHandler = () => {
+    let emptyUser = {};
+
+    this.props.navigation.push("Profile", {
+      user: null,
+      isNewContact:this.props.isNewContact,
+      checkUpdates: this.props.checkUpdates,
+    });
+  };
+
+  render() {
+    return (
+      <TouchableOpacity
+        activeOpacity={0.7}
+        onPress={this.clickHandler}
+        style={[styles.TouchableOpacityStyle]}>
+        <Icon
+          name="add"
+          color={'#fff'}
+          style={styles.FloatingButtonStyle}
+        />
+      </TouchableOpacity>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  MainContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  TouchableOpacityStyle: {
+    position: 'absolute',
+    width: 50,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    right: 30,
+    bottom: 30,
+    alignSelf: 'flex-end',
+    backgroundColor: '#000',
+    overflow: 'hidden',
+    borderRadius: 100
+  },
+
+  FloatingButtonStyle: {
+    resizeMode: 'contain',
+    width: 30,
+    height: 30,
+    color: '#fff'
+    //backgroundColor:'black'
+  },
+});
